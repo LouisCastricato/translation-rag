5fa5f49b1966318edd326d1c929277666fb603d7
LouisCastricato <wirezapp@gmail.com>
2022-05-09 20:41:45 +0000

    RAG trains :D

--- RAG/train.py
+++ RAG/train.py
@@ -9,29 +9,18 @@ from torch.utils.data import DataLoader
 def train(model, dataset_loader, epochs=10):
     optim = torch.optim.AdamW(model.parameters(), lr=5e-4)
     for _ in (pbar := tqdm(range(epochs))):
-        for contrastive_batch in dataset_loader:
-            # source lang is bs x embedding_dim
-            # target lang is bs x neg_samples x embedding_dim
+        for batch in dataset_loader:
+            
+            # forward pass
+            model_output =  model(batch)
 
-            #embed
-            embedded_source, embedded_target = model(contrastive_batch)
-
-            def loss(embedded_source, embedded_target):
-                """
-                Implements InfoNCE loss
-                """
-                # compute the logits
-                logits = torch.bmm(embedded_source.unsqueeze(1), embedded_target.transpose(1, 2))
-                # softmax
-                logits = torch.nn.functional.log_softmax(logits, dim=2).squeeze(1)
-
-                # sum along the 0th column
-                return -logits[:, 0].mean()
-
-            loss_value = loss(embedded_source, embedded_target)
+            # loss and backwards step
+            loss_value = model_output['Loss/Total']
             loss_value.backward()
             optim.step()
             optim.zero_grad()
+
+        # update tqdm bar every batch with the loss
         pbar.set_description(f"loss: {loss_value}")
 
 if __name__ == "__main__":
@@ -52,14 +41,9 @@ if __name__ == "__main__":
     # load the DPR model
     model.dpr.load_state_dict(torch.load('DPR_encoder.pt'))
 
-    # compute a forward pass
-    for data in dataloader:
-        print(model(data))
-        import sys
-        sys.exit()
-    with torch.no_grad():
-        print(dataset_train[:10])
-        print(model(dataset_train[:10]))
+    # train the model
+    train(model, dataloader)
 
-    #train(model, dataloader)
+    # save the model
+    torch.save(model.state_dict(), 'RAG.pt')
 
