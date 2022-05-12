e842eeb03b906f0aa0c2e365fba4ae2d738a9a77
LouisCastricato <wirezapp@gmail.com>
2022-05-12 17:42:23 +0000

    Have modal collapse diagnostics working but its a bit odd...

--- RAG/visualization_util.py
+++ RAG/visualization_util.py
@@ -3,37 +3,60 @@
 from modalcollapse.utils import *
 from modalcollapse.indexing.faiss_utils import singular_value_plot_faiss, batch
 from tqdm import tqdm
+from functools import partial 
 
 # this function takes a list of datasets and produces local SVD plots for each
 def get_intrinsic_dimension_plot(datasets):
     batched = batch(datasets)
 
     def cosine_filter_condition(pt1, pt2):
-        return (np.dot(pt1, pt2) / (np.linalg.norm(pt1) * np.linalg.norm(pt2)) > 0.2)
+        return (np.dot(pt1, pt2) / (np.linalg.norm(pt1) * np.linalg.norm(pt2)) > 0.0)
 
     # build indices
     print("building indices.")
     indexes = [batched(t) for t in tqdm(range(len(datasets)))]
     # get singular values
     print("computing singular values.")
+    #filtered_singular_value_plot_faiss = partial(singular_value_plot_faiss, filter_condition=cosine_filter_condition)
     return list(map(singular_value_plot_faiss, tqdm(indexes)))
 
 # returns the AUC of min(get_intrinsic_dimension_plot(datasets))
 def get_intrinsic_dimension_auc(datasets):
-    svl = np.array(get_intrinsic_dimension_plot(datasets))
+    # get intrinsic dimension plot
+    svl = np.asarray(get_intrinsic_dimension_plot(datasets), dtype=np.float64)
 
-    def get_min_svl(dataset):
-        min_svl = [min(s[:, t]) for s in svl]
-        return min_svl
+    # semilogy and abs
+    svl[svl < 0] = 0
+    svl = np.abs(np.log(svl))
+
+    # replace all -infs and NaNs with 0
+    svl[np.isinf(svl)] = 0
+    svl[np.isnan(svl)] = 0
+
+    # for each set of svl curves find the minimum
+    def get_min_svl(x):
+        return [(np.min(s, axis=0)) for s in x]
+
     # get min_svl for each dataset
-    min_svl = np.array(list(map(get_min_svl, datasets)))
+    min_svl = np.array(list(map(get_min_svl, svl)))
+
     # for each graph in min_svl, integrate
     return list(map(np.trapz, min_svl))
 
 # test functionality
 if __name__ == "__main__":
+    mean = [0.] * 20
+    cov = np.eye(20)
+
     # generate a list of 2 randomly initialized numpy arrays
-    datasets = [np.random.rand(10000, 100) for _ in range(2)]
+    datasets = [get_splooch_points(set_size=50000, dim=20) for _ in range(2)]
+    datasets2 = [np.random.multivariate_normal(mean, cov, size=50000)]
+
+    datasets += datasets2
+
+    # convert datasets to float32
+    datasets = [np.array(d, dtype=np.float32) for d in datasets]
+
     # get the AUC of the intrinsic dimension plot
     auc = get_intrinsic_dimension_auc(datasets)
     print(auc)
